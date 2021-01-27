442dedee2da7f0fdef7b10b496a970feea39b6dd
sungyoungPark <sygitter1491012@gmail.com>
2021-01-28 04:55:54 +0900

    이미지 URL 추출 가능

--- rxSwiftStudy1/Service/HTMLParserManager.swift
+++ rxSwiftStudy1/Service/HTMLParserManager.swift
@@ -9,8 +9,8 @@ import UIKit
 
 class HTMLParserManager {
     
-    func getHTML(){
-        let urlPath = "https://news.google.com/__i/rss/rd/articles/CBMiPmh0dHA6Ly93d3cubWVkaWF0b2RheS5jby5rci9uZXdzL2FydGljbGVWaWV3Lmh0bWw_aWR4bm89MjExNjY40gFBaHR0cDovL3d3dy5tZWRpYXRvZGF5LmNvLmtyL25ld3MvYXJ0aWNsZVZpZXdBbXAuaHRtbD9pZHhubz0yMTE2Njg?oc=5"
+    func getHTML(_ news : News){
+        let urlPath = "https://news.google.com/__i/rss/rd/articles/CBMiMWh0dHBzOi8vd3d3Lnl0bi5jby5rci9fbG4vMDEwMV8yMDIxMDEyNzIyMDgxMjQyMznSAUNodHRwczovL20ueXRuLmNvLmtyL25ld3Nfdmlldy5hbXAucGhwP3BhcmFtPTAxMDFfMjAyMTAxMjcyMjA4MTI0MjM5?oc=5"
         let url = NSURL(string: urlPath)
 
 
@@ -21,8 +21,12 @@ class HTMLParserManager {
             (data, respons, error) -> Void in
             
             if error == nil{
-                let urlContent = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+                let urlContent = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)?.components(separatedBy: ["\n","\t"])
+                    .filter { $0.range(of: "og:image") != nil }
+                    .filter{ $0.range(of: "http") != nil }.map{ String($0).hrefUrl }
+                
                 print(urlContent ?? "no contents found")
+        
             } else {
                 print("Eror 발생")
             }
@@ -32,4 +36,26 @@ class HTMLParserManager {
         task.resume()
     }
     
+}
+
+extension String {
+  func getUrl() -> String? {
+      let rss = self.split { (char) -> Bool in
+          return char == ">"
+      }
+      if let final = rss.last?.split(separator: "<"), let first = final.first {
+          return String(first)
+      }
+      return nil
+  }
+
+  var hrefUrl: String {
+    let matchString = "=\""
+    let arrComponents = self.components(separatedBy: matchString)
+    if let first = arrComponents.last, let str = first.split(separator: "\"").first {
+
+        return String(str)
+    }
+    return ""
+  }
 }
